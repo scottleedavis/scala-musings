e9c4ce3c28fdbd2bb2da8ccc9dc0029c2995a87f
Scott Davis <scott.davis@webtrends.com>
2014-11-20 16:31:03 -0800

    added function programming in scala code base as submodule.

---
+++ match_expression.scala
@@ -0,0 +1,14 @@
+// import fpinscala.datastructures
+//ARg!!!! how to import relative path
+import _root_.fpinscala.answers.src.main.scala.fpinscala.datastructures._
+
+
+var x = List(1,2,3,4,5) match {
+  case Cons(x, Cons(2, Cons(4,_))) => x
+  case Nil => 42
+  case Cons(x, Cons(y, Cons(3, Cons(4,_)))) => x + y
+  case Cons(h, t) => h + sum(t)
+  case _ => 101
+}
+
+println(x)
